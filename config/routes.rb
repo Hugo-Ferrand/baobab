6f35659eb53529a93976d1eb2a72cae85ccba499
Sarah Guillon <sarah.guillon.11@gmail.com>
2022-02-21 16:40:09 +0100

    routes config

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,19 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :trees, only: [:show, :index] do
+    resources :bookings, only: [:create]
+  end
+
+  resource :dashboard, only: [:show]
+
+  namespace :owner do
+    resources :trees, only: [:new, :create, :edit, :update, :destroy]
+    resource :dashboard, only: [:show]
+    resources :bookings do
+      member do
+        patch :decline, :accept
+      end
+    end
+  end
 end
