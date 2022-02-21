987beb3fbcc4c5069c79ca6ac464b47c1fdd69af
matthias seeburger <seeburger.matthias@gmail.com>
2022-02-21 16:42:00 +0100

    Addition of validations for the models

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :bookings
+  has_many :trees
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true
+  validates :encrypted_password, presence: true
+
 end
