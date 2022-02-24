7c70b146bf19a69fe447d7c737220475c2e10503
Hugo Ferrand <hugo.ferrand98@audencia.com>
2022-02-24 16:31:39 +0100

    dropdown log in log out

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,18 +6,19 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import "bootstrap"
 
-Rails.start()
-Turbolinks.start()
-ActiveStorage.start()
 
 import "controllers"
-import "bootstrap"
 
 // ./packs/application.js
 import { Application } from 'stimulus'
 import { definitionsFromContext } from 'stimulus/webpack-helpers'
 import Flatpickr from 'stimulus-flatpickr'
+
+Rails.start()
+Turbolinks.start()
+ActiveStorage.start()
 
 const application = Application.start()
 const context = require.context('../controllers', true, /\.js$/)
