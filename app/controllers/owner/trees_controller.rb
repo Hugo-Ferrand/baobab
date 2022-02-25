9f4e876e6edc822a7506116271cbaa7de6e76fc8
Sarah Guillon <sarah.guillon.11@gmail.com>
2022-02-25 15:05:31 +0100

    create a baobab

--- app/controllers/owner/trees_controller.rb
+++ app/controllers/owner/trees_controller.rb
@@ -4,6 +4,20 @@ module Owner
       @tree = Tree.new
     end
 
+    def create
+      @tree = Tree.new(tree_params)
+      @tree.user = current_user
+      if @tree.save
+        redirect_to tree_path(@tree)
+      else
+        render :new
+      end
+    end
 
+    private
+
+    def tree_params
+      params.require(:tree).permit(:species, :price, :street, :city, :countryname, :description, :photo)
+    end
   end
 end
