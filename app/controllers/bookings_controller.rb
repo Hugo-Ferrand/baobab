e6fde92ce740f7024bfdcd8af7ee6be9be8ca211
matthias seeburger <seeburger.matthias@gmail.com>
2022-02-25 09:49:44 +0100

    Better dashboard with css and smileys

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -5,16 +5,21 @@ class BookingsController < ApplicationController
     @renter_created_bookings = Booking.includes(:tree).where.not(tree: { user: current_user }).where(status: "created", user: current_user)
     @renter_validated_future_bookings = Booking.includes(:tree).where.not(tree: { user: current_user }).where(status: "validated", user: current_user).where('start_date > ?', DateTime.now)
     @renter_validated_past_bookings = Booking.includes(:tree).where.not(tree: { user: current_user }).where(status: "validated", user: current_user).where('end_date < ?', DateTime.now)
+    @renter_declined_future_bookings = Booking.includes(:tree).where.not(tree: { user: current_user }).where(status: "declined", user: current_user).where('start_date > ?', DateTime.now)
   end
 
   def create
     @booking = Booking.new(booking_params)
+    if @booking.end_date.nil? || @booking.start_date.nil? || current_user.nil?
+      return redirect_to new_user_session_path
+    end
+
     @tree = Tree.find(params[:tree_id])
     @booking.tree = @tree
     @booking.user = current_user
     @booking.total_price = @tree.price * (@booking.end_date - @booking.start_date)
     @booking.status = "created"
-    
+
     if @booking.save
 
       redirect_to dashboard_path
